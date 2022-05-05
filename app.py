2a0540d148a685383b70846ab32df7a70fe724e5
HirojiFukuyama <kuangyuxuan123@gmail.com>
2022-05-05 09:28:03 +0800

    add cache

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from gensim.models import Word2Vec
 from gen import gen
 import time
 
+@st.cache
+def load_model(model_path, wv_path):
+    return torch.load(model_path, map_location='cpu'), Word2Vec.load(wv_path).wv
+
 # last last last one: "lgg_model_paths/hole-merge_lstm_drop_2022-05-02_01_19_40"
 # last last one: "lgg_model_paths/hole-merge_input30_2022-05-02_13_24_14"
 # last one: "lgg_model_paths/merge_input50"
-model = torch.load("lgg_model_paths/merge_input30_512", map_location='cpu') # input50
+model, wv = load_model("lgg_model_paths/merge_input30_512", "word_model_paths/hole-merge") # large
 model.eval()
-wv = Word2Vec.load("word_model_paths/hole-merge").wv
-
 
 st.title("P大树洞-爱の引论")
 st.subheader("欢迎来到P大树洞！@HoleAI")
@@ -19,27 +21,25 @@ choice = st.radio("选择一个模型", ("HoleAI-small", "HoleAI-medium", "HoleA
 
 if choice == "HoleAI-small":
     with st.spinner("载入模型中..."):
-        model = torch.load("lgg_model_paths/hole-merge_2022-05-01_04_53_11", map_location='cpu')
+        model, wv = load_model("lgg_model_paths/hole-merge_2022-05-01_04_53_11", "word_model_paths/hole-merge")
         model.eval()
-        wv = Word2Vec.load("word_model_paths/hole-merge").wv
 
 elif choice == "HoleAI-medium":
     with st.spinner("载入模型中..."):
-        model = torch.load("lgg_model_paths/merge_input50", map_location='cpu')
+        model, wv = load_model("lgg_model_paths/merge_input50", "word_model_paths/hole-merge")
         model.eval()
-        wv = Word2Vec.load("word_model_paths/hole-merge").wv
 
 elif choice == "HoleAI-large":
     with st.spinner("载入模型中..."):
-        model = torch.load("lgg_model_paths/merge_input30_512", map_location='cpu') # only 20 epochs
+        model, wv = load_model("lgg_model_paths/merge_input30_512", "word_model_paths/hole-merge") # 20 epochs
         model.eval()
-        wv = Word2Vec.load("word_model_paths/hole-merge").wv
 
 elif choice == "HoleAI-ultra":
     with st.spinner("载入模型中..."):
-        model = torch.load("lgg_model_paths/hole-merge_30_512_3_50_2022-05-04_03_17_17", map_location='cpu') # 50 epochs
+        model, wv = load_model("lgg_model_paths/hole-merge_30_512_3_50_2022-05-04_03_17_17", "word_model_paths/hole-merge") # 50 epochs
+        model.eval()
 
-count = st.number_input("选择回复数量", 1, 20, 5, 1)
+count = st.number_input("选择树洞长度", 1, 20, 5, 1)
 
 dz = st.text_input("发一条树洞吧！")
 
