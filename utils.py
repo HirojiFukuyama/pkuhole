547909e2f141f30e8b498d8446967a4dc3ffc386
HirojiFukuyama <kuangyuxuan123@gmail.com>
2022-05-21 14:18:10 +0800

    knowledge

--- utils.py
+++ utils.py
@@ -50,8 +50,8 @@ def gen(model, wv, dz, flag, count=5, device='cpu'):
 
 def preprocess(dz, emotion, wv):
     # "knowledge"
-    positive = ['抱抱', 'patpat', '摸摸', 'dz不哭', '呜呜', '哈哈哈哈', '笑死', '嘎嘎', '同感', '哦', '突突突突突突突突', '可爱捏', '恭喜', '哎呀', wv.index_to_key[1713]+'\n', wv.index_to_key[1456]+'\n', wv.index_to_key[1900]+'\n', wv.index_to_key[1111]+'\n', '正确的', '3.92\n', 'dz好棒！', 'www', '哇', '一眼丁真 ', '冲！', '蹲 ', '难蚌\n', '蚌埠住了\n', '正确的 ']
-    negative = ['1/10', wv.index_to_key[273]+'\n', '呵呵', '举报了', '寄\n', '寄了\n', '急了急了', '呃', '典\n', '典中典\n', '麻了', '钝角\n', '怎么会事呢\n', '哈人 ', wv.index_to_key[1343]+'\n', wv.index_to_key[1441]+'\n', wv.index_to_key[2679]+'\n', wv.index_to_key[1566]+'\n', wv.index_to_key[2339]+'\n', '爬\n', '出吗\n', "哼哼哼啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n"]
+    positive = ['抱抱', 'patpat', '摸摸', 'dz不哭', '呜呜', '哈哈哈哈', '笑死', '嘎嘎', '同感', '哦', '突突突突突突突突', '可爱捏', '恭喜', '哎呀', wv.index_to_key[1713]+'\n', wv.index_to_key[1456]+'\n', wv.index_to_key[1900]+'\n', wv.index_to_key[1111]+'\n', '留号\n', '3.92\n', 'dz好棒！', 'www', '哇', '一眼丁真 ', '冲！', '蹲 ', '难蚌\n', '蚌埠住了\n', '正确的 ', '加油！']
+    negative = ['1/10', wv.index_to_key[273]+'\n', '呵呵', '举报了', '寄\n', '寄了\n', '急了急了', '呃', '典\n', '典中典\n', '麻了', '钝角\n', '怎么会事呢\n', '哈人 ', wv.index_to_key[1343]+'\n', wv.index_to_key[1441]+'\n', wv.index_to_key[2679]+'\n', wv.index_to_key[1566]+'\n', wv.index_to_key[2339]+'\n', '爬\n', '出吗\n', "哼哼哼啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n", 'nhml\n']
 
     dz += "\n[Alice]"
     if emotion == 'positive':
