1125ed25a5e8c7f07de5e7142823c7f1ce4a5886
忘忧北萱草 <1779724477@qq.com>
2022-06-09 08:30:07 +0000

    优化 streamlit 交互

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from gensim.models import Word2Vec
 from utils import *
 import time
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 st.set_page_config(
-    page_title="P大树洞-爱の引论", 
+    page_title="P大树洞-爱の引论",
     page_icon="docs/assets/hole_icon",
     layout="centered",
     initial_sidebar_state="expanded",
@@ -16,13 +18,26 @@ st.set_page_config(
     }
 )
 
-@st.cache
+
+def state_cache(func):
+    def wrapped(arg):
+        cache_id = func.__name__ + str(arg)
+        if cache_id not in st.session_state:
+            st.session_state[cache_id] = func(arg)
+        return st.session_state[cache_id]
+    return wrapped
+
+
+@state_cache
 def load_wv(wv_path):
     return Word2Vec.load(wv_path).wv
 
-@st.cache
+@state_cache
 def load_model(model_path):
-    return torch.load(model_path, map_location='cpu')
+    with st.spinner("载入模型中..."):
+        model = torch.load(model_path, map_location=device)
+        model.eval()
+        return model
 
 st.title("P大树洞-爱の引论")
 st.subheader("欢迎来到P大树洞！@HoleAI")
@@ -30,55 +45,57 @@ choice = st.sidebar.radio("选择一个模型", ("HoleAI-small", "HoleAI-medium"
 emotion = st.sidebar.radio("选择一个情绪", ("neutral", "positive", "negative"))
 
 
+model_paths = {
+    "HoleAI-small": "lgg_model_paths/new/hole-merge_30_256_3_50_2022-06-03_06_55_44",
+    "HoleAI-medium": "lgg_model_paths/new/hole-merge_30_512_2_50_2022-06-03_06_57_00",
+    "HoleAI-large": "lgg_model_paths/new/hole-merge_30_512_3_50_2022-06-03_03_19_13",
+    "HoleAI-ultra": "lgg_model_paths/new/hole-merge_30_512_4_50_2022-06-03_06_54_54"
+}
+
+class ProgressBar:
+    def __init__(self, total):
+        self._pbar = st.progress(0)
+        self._total = total
+        self._count = 0
+
+    def update(self, count=1):
+        self._count += count
+        if self._count >= self._total * 0.9:
+            self._count = self._total * 0.9
+        self._pbar.progress(int(self._count / self._total * 100))
+
+    def finish(self):
+        self._pbar.progress(100)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.finish()
+
 def main():
-    flag = False
     count = st.sidebar.number_input("选择树洞长度", 1, 20, 5, 1)
     dz = st.text_input("发一条树洞吧！")
-    if dz:
-        flag = True
 
-    if st.button("开始生成"):
+    if st.button("开始生成") and dz:
         wv = load_wv("word_model_paths/hole_new") # load the word model only once
         wv2 = load_wv("word_model_paths/hole-merge") # original
         dz = preprocess(dz, emotion, wv2)
 
-        if choice == "HoleAI-small":
-            with st.spinner("载入模型中..."):
-                model = load_model("lgg_model_paths/new/hole-merge_30_256_3_50_2022-06-03_06_55_44")
-                model.eval()
+        model = load_model(model_paths.get(choice, 'large'))
 
-        elif choice == "HoleAI-medium":
-            with st.spinner("载入模型中..."):
-                model = load_model("lgg_model_paths/new/hole-merge_30_512_2_50_2022-06-03_06_57_00")
-                model.eval()
-
-        elif choice == "HoleAI-large":
-            with st.spinner("载入模型中..."):
-                model = load_model("lgg_model_paths/new/hole-merge_30_512_3_50_2022-06-03_03_19_13")
-                model.eval()
-
-        elif choice == "HoleAI-ultra":
-            with st.spinner("载入模型中..."):
-                model = load_model("lgg_model_paths/new/hole-merge_30_512_4_50_2022-06-03_06_54_54")
-                model.eval()
-
-        else:
-            with st.spinner("载入模型中..."):
-                model = load_model("lgg_model_paths/new/hole-merge_30_512_3_50_2022-06-03_03_19_13") # large
-                model.eval()
-
-        with st.spinner("生成中..."):
+        with ProgressBar(total=count*50) as pbar:
             start_time = time.time()
-            out = gen(model, wv, dz, flag, count)
+            st.session_state['out'] = gen(model, wv, dz, count, device=device, step_callback=pbar.update)
+            st.balloons()
 
-        if out:
-            out = out.split("\n")
-            for i in out:
+        if 'out' in st.session_state:
+            out = st.session_state['out'] or "没有生成结果"
+            for i in out.split("\n"):
                 if i != '':
                     st.info(i)
             end_time = time.time()
             st.success("本次生成耗时：{:.4f}秒".format(end_time-start_time))
-            st.balloons()
 
 
 if __name__ == "__main__":
